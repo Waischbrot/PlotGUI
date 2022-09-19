2a53cf65aa7f21c412757b20676501ecddb3ff33
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2022-09-19 08:27:45 +0200

    updated MainMenu

--- src/main/java/de/waischbrot/plotgui/gui/MainMenu.java
+++ src/main/java/de/waischbrot/plotgui/gui/MainMenu.java
@@ -2,14 +2,20 @@ package de.waischbrot.plotgui.gui;
 
 import de.waischbrot.libraries.invlib.basic.RubyInventory;
 import de.waischbrot.libraries.messages.StringUtil;
-import de.waischbrot.plotgui.Main;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class MainMenu extends RubyInventory {
 
-    public MainMenu(@NotNull Player player) {
+    public MainMenu(final @NotNull Player player) {
         super(player, "main", StringUtil.color("&8Hauptmenü"), 5);
+    }
+
+    @Override
+    public void handleInventoryOpenEvent(final InventoryOpenEvent event) {
+
+
+
     }
 }
