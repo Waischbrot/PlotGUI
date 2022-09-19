333a9948d80fd7b546d3bfd3bec2a54c4928d272
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2022-09-19 08:42:56 +0200

    current state

--- src/main/java/de/waischbrot/plotgui/gui/MainMenu.java
+++ src/main/java/de/waischbrot/plotgui/gui/MainMenu.java
@@ -1,7 +1,11 @@
 package de.waischbrot.plotgui.gui;
 
+import de.waischbrot.libraries.invlib.basic.Button;
 import de.waischbrot.libraries.invlib.basic.RubyInventory;
+import de.waischbrot.libraries.invlib.basic.SlotPosition;
+import de.waischbrot.libraries.itembuilder.ItemBuilder;
 import de.waischbrot.libraries.messages.StringUtil;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.jetbrains.annotations.NotNull;
@@ -15,6 +19,14 @@ public class MainMenu extends RubyInventory {
     @Override
     public void handleInventoryOpenEvent(final InventoryOpenEvent event) {
 
+        fillBorders(new Button(new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE)
+                .setDisplayName(StringUtil.color("&8"))
+                .build()));
+
+        addButton(SlotPosition.of(2, 4), new Button(new ItemBuilder(Material.PLAYER_HEAD)
+                .getSkullBase("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTJkN2E3NTFlYjA3MWUwOGRiYmM5NWJjNWQ5ZDY2ZTVmNTFkYzY3MTI2NDBhZDJkZmEwM2RlZmJiNjhhN2YzYSJ9fX0=")
+                .setDisplayName(StringUtil.color("&b" + player.getName()))
+                .build()));
 
 
     }
